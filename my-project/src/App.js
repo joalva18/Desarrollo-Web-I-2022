f900c5b600a3858dccf92d2a66d81e60385c4872
joalva18 <50782609+joalva18@users.noreply.github.com>
2022-04-23 17:43:49 -0600

    Add files via upload

---
+++ my-project/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Tarea 01</p>
+        <p>Johan Murillo Alvarado</p>
+        <p>Carnet: B64918</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
